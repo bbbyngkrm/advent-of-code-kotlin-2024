3dd5f1a3b334d552b22b00b5be7a757845b6e355
Bobby Ingkirama <bobby.ingkirama@bytedance.com>
2024-12-02 15:12:58 +0700

    finish day 2

---
+++ src/Day02.kt
@@ -0,0 +1,36 @@
+fun main() {
+    fun part1(inputs: List<String>): Int {
+        return inputs
+            .map { it.split(" ").map { it.toInt() } }
+            .count { isSafe(it) }
+    }
+
+    fun part2(inputs: List<String>): Int {
+        return inputs
+            .map { it.split(" ").map { it.toInt() } }
+            .count {
+                if (isSafe(it)) {
+                    return@count true
+                } else {
+                    it.forEachIndexed { index, _ ->
+                        val new = it.toMutableList()
+                        new.removeAt(index)
+                        if (isSafe(new)) return@count true
+                    }
+                    return@count false
+                }
+            }
+    }
+
+    // Read the input from the `src/Day02.txt` file.
+    val input = readInput("Day02")
+    part1(input).println()
+    part2(input).println()
+}
+
+fun isSafe(inputs: List<Int>): Boolean {
+    val result = inputs.windowed(2).map { it[1] - it[0] }
+    val increasing = result.all { it in 1..3 }
+    val decreasing = result.all { it in -1 downTo -3 }
+    return increasing || decreasing
+}
